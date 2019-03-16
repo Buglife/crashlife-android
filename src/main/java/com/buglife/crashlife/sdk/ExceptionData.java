f6ff8906c3d9dd8668c24dfd808ae5b629b7a38d
Daniel DeCovnick <danhd123@mac.com>
2019-03-15 20:42:33 -0700

    more constantifying

--- src/main/java/com/buglife/crashlife/sdk/ExceptionData.java
+++ src/main/java/com/buglife/crashlife/sdk/ExceptionData.java
@@ -28,6 +28,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 final class ExceptionData implements JSONCaching {
+    private static final String MESSAGE = "message";
+    private static final String LOCALIZED_MESSAGE = "localized_message";
+    private static final String EXCEPTION_CLASS = "exception_class";
+    private static final String STACK_FRAMES = "stack_frames";
     private final String mMessage;
     private final String mLocalizedMessage;
     private final String mExceptionClass;
@@ -81,10 +85,10 @@ final class ExceptionData implements JSONCaching {
     public JSONObject toCacheJson() {
         JSONObject result = new JSONObject();
 
-        JsonUtils.safePut(result,"message", getMessage());
-        JsonUtils.safePut(result,"localized_message", getLocalizedMessage());
-        JsonUtils.safePut(result,"exception_class", getExceptionClass());
-        JsonUtils.safePut(result,"stack_frames", JsonUtils.listToCacheJson(mStackframes));
+        JsonUtils.safePut(result, MESSAGE, getMessage());
+        JsonUtils.safePut(result, LOCALIZED_MESSAGE, getLocalizedMessage());
+        JsonUtils.safePut(result, EXCEPTION_CLASS, getExceptionClass());
+        JsonUtils.safePut(result, STACK_FRAMES, JsonUtils.listToCacheJson(mStackframes));
         return result;
     }
 
@@ -95,11 +99,11 @@ final class ExceptionData implements JSONCaching {
         String exceptionClass;
         List<StackFrame> stackFrames = new ArrayList<>();
 
-        message = JsonUtils.safeGetString(jsonObject,"message");
-        localizedMessage = JsonUtils.safeGetString(jsonObject,"localized_message");
-        exceptionClass = JsonUtils.safeGetString(jsonObject,"exception_class");
+        message = JsonUtils.safeGetString(jsonObject, MESSAGE);
+        localizedMessage = JsonUtils.safeGetString(jsonObject, LOCALIZED_MESSAGE);
+        exceptionClass = JsonUtils.safeGetString(jsonObject, EXCEPTION_CLASS);
 
-        JSONArray stackframesJson = JsonUtils.safeGetJSONArray(jsonObject,"stack_frames");
+        JSONArray stackframesJson = JsonUtils.safeGetJSONArray(jsonObject, STACK_FRAMES);
         if (stackframesJson != null) {
             stackFrames = StackFrame.listFromCacheJson(stackframesJson);
         }
